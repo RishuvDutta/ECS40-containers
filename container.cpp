e7de9b37e229daed13706695f957b41269efa452
Rishi Dutta <rdutta1013@gmail.com>
2016-12-14 16:02:03 -0800

    Add files via upload

---
+++ container.cpp
@@ -0,0 +1,17 @@
+#include "container.h"
+
+
+Container::Container(int siz) : size(siz) 
+{
+} // Container()
+
+Container::~Container()
+{
+} // ~Container()
+
+
+int Container::getSize() const
+{
+  return size;
+} // getSize()
+
